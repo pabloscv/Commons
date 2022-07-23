588460823911dedd657078262da6619a3c106665
pabloscv <pabloscv@gmail.com>
2022-07-23 14:05:13 +0000

    Primeira versão

---
+++ InstallSVC.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+clear
+echo "Atualizando o sistema"
+sudo apt-get -y update
+sudo apt -y full-upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y update
+echo "Instalando ASP.NET Core 6"
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get -y update
+sudo apt-get install -y apt-transport-https
+sudo apt-get -y update
+sudo apt-get install -y dotnet-sdk-6.0
+sudo dotnet dev-certs https --trust
+echo "Instalando REDIS"
+sudo apt-get -y install redis
+echo "Instalando POSTGRES"
+sudo apt-get -y install postgresql
+echo "Instalando Serviços"
+sudo cp ./ServerRecoveryLinux/ServerRecoveryLinux.service /etc/systemd/system/
+sudo cp ./AuthorizationLinux/AuthorizationLinux.service /etc/systemd/system/
+sudo cp ./SettingsDriverLinux/SettingsDriverLinux.service /etc/systemd/system/
+sudo cp ./SettingsWebInterfaceLinux/SettingsWebInterfaceLinux.service /etc/systemd/system/
+echo "Registrando Serviços"
+sudo systemctl enable ServerRecoveryLinux.service
+sudo systemctl enable AuthorizationLinux.service
+sudo systemctl enable SettingsDriverLinux.service
+sudo systemctl enable SettingsWebInterfaceLinux.service
+echo "Iniciando Serviços"
+sudo service SettingsWebInterfaceLinux start
+sudo service SettingsDriverLinux start
+sudo service ServerRecoveryLinux start
+sudo service SettingsWebInterfaceLinux start
+echo "Instalando Apache2"
+sudo apt-get -y install apache2
+sudo service apache2 stop
+sudo a2enmod headers
+sudo a2enmod ssl
+sudo a2enmod proxy
+sudo a2enmod proxy_balancer
+sudo a2enmod proxy_http
+sudo a2enmod http2
+sudo a2dissite 000-default.conf
+sudo cp ./ProxyReverse.conf /etc/apache2/sites-available/
+sudo a2ensite ProxyReverse.conf
+sudo service apache2 start
+sudo service apache2 status
+
---
+++ SettingsDriverLinux/README.md
@@ -0,0 +1,10 @@
+- 👋 Olá sou Pablo Cavalcante
+- 👀 Estou criando esse repositório para guardar ideias e criações
+- 🌱 Atualmente estou estudando as tecnologias de dados
+- 💞️ Atualmente sou colaborador do Laboratório de Tecnologia Jurídica da OAB/MS
+- 📫 Para falar comigo envie um email para pabloscv@gmail.com
+
+<!---
+pabloscv/pabloscv is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
