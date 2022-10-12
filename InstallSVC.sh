1ffdad316bc5f54e5f77e6cb391fb8da0bd516d5
Pablo S. Cavalcante <pabloscv@gmail.com>
2022-10-12 10:38:21 -0400

    atualização

--- InstallSVC.sh
+++ InstallSVC.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
 clear
-echo "^[[32m^Atualizando o sistema[[0m"
+echo -e "\e[32m Atualizando o sistema \e[0m"
 sudo apt-get -y update
 sudo apt -y full-upgrade
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y update
-echo "^[[32m^Instalando ASP.NET Core 6[[0m"
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get -y update
+echo -e "\e[32m Instalando ASP.NET Core 6 \e[0m"
 sudo apt-get install -y apt-transport-https
 sudo apt-get -y update
 sudo apt-get install -y dotnet-sdk-6.0
 sudo dotnet dev-certs https --trust
-echo "^[[32m^Instalando REDIS[[0m"
+echo -e "\e[32m Instalando REDIS \e[0m"
 sudo apt-get -y install redis
-echo "^[[32m^Instalando POSTGRES[[0m"
+echo -e "\e[32m Instalando POSTGRES \e[0m"
 sudo apt-get -y install postgresql
-echo "^[[32m^Instalando SSMTP[[0m"
+echo -e "\e[32m Instalando SSMTP \e[0m"
 sudo apt-get -y install ssmtp
 sudo rm -rf /etc/ssmtp/ssmtp.conf
 sudo cp ./ssmtp.conf /etc/ssmtp/
-echo "^[[32m^Instalando Serviços[[0m"
-sudo cp ./ServerRecoveryLinux/ServerRecoveryLinux.service /etc/systemd/system/
-sudo cp ./AuthorizationLinux/AuthorizationLinux.service /etc/systemd/system/
-sudo cp ./SettingsDriverLinux/SettingsDriverLinux.service /etc/systemd/system/
-sudo cp ./SettingsWebInterfaceLinux/SettingsWebInterfaceLinux.service /etc/systemd/system/
-echo "^[[32m^Registrando Serviços[[0m"
+echo -e "\e[32m Instalando Serviços \e[0m"
+sudo cp ./LinuxServices/ServerRecoveryLinux.service /etc/systemd/system/
+sudo cp ./LinuxServices/AuthorizationLinux.service /etc/systemd/system/
+sudo cp ./LinuxServices/SettingsDriverLinux.service /etc/systemd/system/
+sudo cp ./LinuxServices/SettingsWebInterfaceLinux.service /etc/systemd/system/
+echo -e "\e[32m Registrando Serviços \e[0m"
 sudo systemctl enable ServerRecoveryLinux.service
 sudo systemctl enable AuthorizationLinux.service
 sudo systemctl enable SettingsDriverLinux.service
 sudo systemctl enable SettingsWebInterfaceLinux.service
-echo "^[[32m^Iniciando Serviços[[0m"
+echo -e "\e[32m Iniciando Serviços \e[0m"
 sudo service SettingsWebInterfaceLinux start
 sudo service SettingsDriverLinux start
 sudo service ServerRecoveryLinux start
 sudo service AuthorizationLinux start
-echo "^[[32m^Instalando Apache2[[0m"
+echo -e "\e[32m Instalando Apache2 \e[0m"
 sudo apt-get -y install apache2
-sudo service apache2 stop
 sudo a2enmod headers
 sudo a2enmod ssl
 sudo a2enmod proxy
@@ -48,7 +45,8 @@ sudo a2enmod http2
 sudo a2dissite 000-default.conf
 sudo cp ./ProxyReverse.conf /etc/apache2/sites-available/
 sudo a2ensite ProxyReverse.conf
-sudo service apache2 start
-echo "^[[32m^Instalação concluída[[0m"
-echo "^[[32m^** Lembre-se de terminar a configuração do arquivo /etc/ssmtp/ssmtp.conf[[0m"
+sudo service apache2 restart
+echo -e "\e[32m Instalação concluída \e[0m"
+echo ""
+echo -e "\e[32m ** Lembre-se de terminar a configuração do arquivo /etc/ssmtp/ssmtp.conf \e[0m"
 echo ""
--- README.md
+++ README.md
@@ -1,37 +1,7 @@
-[![.NET](https://github.com/pabloscv/Patty-Authorization-API/actions/workflows/dotnet.yml/badge.svg)](https://github.com/pabloscv/Patty-Authorization-API/actions/workflows/dotnet.yml) [![.NET](https://github.com/pabloscv/ServerRecovery/actions/workflows/dotnet.yml/badge.svg)](https://github.com/pabloscv/ServerRecovery/actions/workflows/dotnet.yml) [![.NET](https://github.com/pabloscv/SettingsDriverCore/actions/workflows/dotnet.yml/badge.svg)](https://github.com/pabloscv/SettingsDriverCore/actions/workflows/dotnet.yml) [![.NET](https://github.com/pabloscv/SettingsDriver/actions/workflows/dotnet.yml/badge.svg)](https://github.com/pabloscv/SettingsDriver/actions/workflows/dotnet.yml)
-
 - 👋 Olá sou Pablo Cavalcante
 - 👀 Estou criando esse repositório para guardar ideias e criações
-- 🌱 Atualmente estou estudando as tecnologias de dados
+- 🌱 Atualmente estou estudando DEVOPS
 - 📫 Para falar comigo envie um email para pabloscv@gmail.com
-
-** Instalação
-
-** Instalação do Postgresql
-* Mude a senha do usuário postgres. sudo passwd postgres
-* Acesse o usuário. su postgres
-* Volte na home. cd ~
-* Acesse o cliente do postgres. psql
-* Troque a senha do banco de dados. ALTER USER postgres WITH PASSWORD 'novasenha';
-* Crie uma database. CREATE DATABASE basedados;
-* Feche a interface cliente. Exit;
-* Feche o usuário. Exit
-* Caso queira acessar o banco de dados fora do servidor então configure o arquivo. pg_hba.conf, utilize IPV4, host all all 0.0.0.0/0 md5
-* Caso queira acessar o banco de dados fora do servidor então configure o aqruivo. postgresql, utilize, listen_addresses = '*'
-
-** Instalação Ubuntu 22LTS ou >
-* Clone o repositório na pasta /var/publish
-* Execute o comando chmod a+x InstallSVC.sh
-* Execute o arquivo /var/publish/Commons/InstallSVC.sh
-* Configure o arquivo /etc/ssmtp/ssmtp.conf ou se precisar utilize o modelo em /var/publish/Commons/ssmtp_modelo2.conf
-* Acesse o endereço do seu site: http://site/settwebi/
-* Acesse o Settings Web Interface e informe um usuário e senha. Esse usuário e senha inicial será gravado como um usuário permanente.
-* Configure a aplicação do SettingsWebInterface
-* Reinicie o servidor
-* Acesse o endereço do seu site: http://site/authorization/configdb
-* Instalado com sucesso!
-
-Obrigado!
 
 <!---
 pabloscv/pabloscv is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
