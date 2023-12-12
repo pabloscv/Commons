c9dda62ed2162fff3709539443b69233571294ba
Pablo S. Cavalcante <ti.litech@oabms.org.br>
2023-12-12 17:05:34 -0400

    nova formatação

--- InstallSVC.sh
+++ InstallSVC.sh
@@ -2,8 +2,6 @@
 clear
 sudo rm -rf /etc/systemd/system/ServerRecoveryLinux.service
 sudo rm -rf /etc/systemd/system/AuthorizationLinux.service
-sudo rm -rf /etc/systemd/system/SettingsDriverLinux.service
-sudo rm -rf /etc/systemd/system/SettingsWebInterfaceLinux.service
 sudo rm -rf /etc/systemd/system/FileServerLinux.service
 echo -e "\e[32m Atualizando o sistema \e[0m"
 sudo apt-get -y update
@@ -28,10 +26,8 @@ sudo a2enmod rewrite
 echo -e "\e[32m Instalando ASP.NET Core 7 \e[0m"
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y dotnet-sdk-7.0
-sudo cd ./SettingsWebInterfaceLinux
 sudo dotnet dev-certs https --clean
 sudo dotnet dev-certs https --trust
-sudo cd ..
 echo -e "\e[32m Instalando REDIS \e[0m"
 sudo apt-get -y install redis
 echo -e "\e[32m Instalando POSTGRES \e[0m"
@@ -41,40 +37,25 @@ sudo apt-get -y install ssmtp
 sudo rm -rf /etc/ssmtp/ssmtp.conf
 sudo cp ./ssmtp.conf /etc/ssmtp/
 echo -e "\e[32m Instalando Serviços \e[0m"
-sudo cp ./LinuxServices/ServerRecoveryLinux.service /etc/systemd/system/
-sudo cp ./LinuxServices/AuthorizationLinux.service /etc/systemd/system/
-sudo cp ./LinuxServices/SettingsDriverLinux.service /etc/systemd/system/
-sudo cp ./LinuxServices/SettingsWebInterfaceLinux.service /etc/systemd/system/
-sudo cp ./LinuxServices/FileServerLinux.service /etc/systemd/system/
-sudo cp -r ./RecursosCompartilhados /var/publish/RecursosCompartilhados
-sudo cp -r ./FileServerData /var/publish/FileServerData
-sudo cp -r ./Applications /var/publish/Applications
+sudo cp ./Configs/LinuxServices/ServerRecoveryLinux.service /etc/systemd/system/
+sudo cp ./Configs/LinuxServices/AuthorizationLinux.service /etc/systemd/system/
+sudo cp ./Configs/LinuxServices/FileServerLinux.service /etc/systemd/system/
 echo -e "\e[32m Registrando Serviços \e[0m"
 sudo systemctl enable ServerRecoveryLinux.service
 sudo systemctl enable AuthorizationLinux.service
-sudo systemctl enable SettingsDriverLinux.service
-sudo systemctl enable SettingsWebInterfaceLinux.service
 sudo systemctl enable FileServerLinux.service
 echo -e "\e[32m Iniciando Serviços \e[0m"
-sudo service SettingsWebInterfaceLinux start
-sudo service SettingsDriverLinux start
 sudo service ServerRecoveryLinux start
 sudo service AuthorizationLinux start
 sudo service FileServerLinux start
 echo -e "\e[32m Configurando o Apache2 \e[0m"
 sudo a2dissite 000-default.conf
-sudo cp ./ProxyReverse.conf /etc/apache2/sites-available/
-sudo cp ./FileReverse.conf /etc/apache2/sites-available/
-sudo cp ./FrontEnd.conf /etc/apache2/sites-available/
+sudo cp ./Configs/Apache2/ProxyReverse.conf /etc/apache2/sites-available/
+sudo cp ./Configs/Apache2/FileReverse.conf /etc/apache2/sites-available/
+sudo cp ./Configs/Apache2/FrontEnd.conf /etc/apache2/sites-available/
 sudo a2ensite ProxyReverse.conf
 sudo a2ensite FileReverse.conf
 sudo a2ensite FrontEnd.conf
 sudo service apache2 restart
 echo -e "\e[32m Instalação concluída \e[0m"
-sudo cp ./iplocal.sh /root/iplocal.sh
-sudo cp ./redis28.conf /root/redis28.conf
-sudo chmod a+x /root/iplocal.sh
-sudo chmod a+x AllRestart.sh
-echo ""
-echo -e "\e[32m ** Lembre-se de terminar a configuração do arquivo /etc/ssmtp/ssmtp.conf \e[0m"
 echo ""
