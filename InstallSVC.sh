0ec759446aa04686caa85aed27a1b801eb9e974f
Pablo S. Cavalcante <ti.litech@oabms.org.br>
2024-04-24 11:21:57 -0400

    atualização

--- InstallSVC.sh
+++ InstallSVC.sh
@@ -26,9 +26,9 @@ sudo a2enmod proxy_balancer
 sudo a2enmod proxy_http
 sudo a2enmod http2
 sudo a2enmod rewrite
-echo -e "\e[32m Instalando ASP.NET Core 7 \e[0m"
+echo -e "\e[32m Instalando ASP.NET Core 8 \e[0m"
 sudo apt-get install -y apt-transport-https
-sudo apt-get install -y dotnet-sdk-7.0
+sudo apt-get install -y dotnet-sdk-8.0
 sudo dotnet dev-certs https --clean
 sudo dotnet dev-certs https --trust
 echo -e "\e[32m Instalando REDIS \e[0m"
